fb3a430a3a8a84c402d010da3ac4bab75eb306a3
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-18 02:19:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,17 +1,9 @@
-Asking the user for their move by position 1-9.
-Receiving the user input.
-Convert position to an index.
-If the move is valid, make the move and display the board to the user.
-If the move is invalid, ask for a new move until a valid move is received.
-
-ask for input
-get input
-convert input to index
-if index is valid
-  make the move for index
-  show the board
-else
-  ask for input again until you get a valid input
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # Define display_board that accepts a board and prints
