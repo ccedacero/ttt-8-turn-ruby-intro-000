709300194c50404c4892d95ebf19b5863ba00035
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-18 02:14:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,3 +3,13 @@ Receiving the user input.
 Convert position to an index.
 If the move is valid, make the move and display the board to the user.
 If the move is invalid, ask for a new move until a valid move is received.
+
+ask for input
+get input
+convert input to index
+if index is valid
+  make the move for index
+  show the board
+else
+  ask for input again until you get a valid input
+end
