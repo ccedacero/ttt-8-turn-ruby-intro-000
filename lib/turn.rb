6f829604d58e7b30636da1d4dfbe7a6c65ae061c
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-18 02:12:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ end
 # code your #position_taken? method here!
 def position_taken?(board, index)
   input_to_index(index)
-  if (board[index] ==" "|| board[index] == "" || board[index] == nil
+  if (board[index] ==" "|| board[index] == "" || board[index] == nil)
     return false;
     elsif board[index] == nil
   return false;
