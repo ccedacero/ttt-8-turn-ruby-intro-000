6874dc2db5ed18f393c55fb810ac31fbd215b062
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-18 02:26:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -49,7 +49,7 @@ end
 # code your #valid_move? method here
 def valid_move?(board,indx)
   index = indx.to_i;
-if index >= 0 && index <= 8 && board[index] == "" || board[index] == " "
+if index >= 0 && index <= 8 && board[index] != 'X' || board[index] == 'Y' 
   return true;
 else
 return false
