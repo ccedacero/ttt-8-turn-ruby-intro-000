dfc7c6fc0104530430499ff09bafafbca4e87150
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-18 02:15:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,13 @@ if index is valid
 else
   ask for input again until you get a valid input
 end
+
+# Define display_board that accepts a board and prints
+# out the current state
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
